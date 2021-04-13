e8a72f8f17a9de0475087b2ba7d62e6728ef07f3
minhhoangbui <minhhoangbui.vn@gmail.com>
2021-04-13 18:59:08 +0700

    check verified

--- dist_train.sh
+++ dist_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=4 \
+python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=2 \
 --master_addr=127.0.0.1 --master_port=29505 \
- train.py -c config.json -d 5,1,3,4 --local_world_size 4
+ train.py -c config.json -d 1,2 --local_world_size 2
 #--resume saved/models/PICK_Default/test_0401_124746/checkpoint-epoch100.pth
